450ea426e968c11de93133d3ef241ebbdd727027
Jacob Reger <regerjacob@gmail.com>
2023-03-12 15:27:43 -0700

    Added skeleton code.

---
+++ assignment_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>     // For input and output
+#include <cmath>        // For pow() function
+#include <cstdlib>      // For rand() function
+#include <ctime>        // For time(0)
+
+// Like I said before, in the future we won't use this next line of code, but for now it's alright to have it.
+using namespace std;
+
+// Hey Ava, I know we have already talked about creating individual functions, but for this first assignment we can
+// just do everything in main for simplicity. It is good coding practice to seperate blocks of code into their own
+// functions, but for this first program it's alright to do it all in main. I've layed out the steps one by one of
+// how I would approach the problem, but you are free to do it in whatever order you feel makes the most sense.
+// You don't need to follow this format. Lemme know if you have any questions :)
+
+int main()
+{
+    // 1. Gather user input
+
+    // 2. Cacluate annual salary
+
+    // 3. Calculate commission from car sales (hint: remember to use rand() to get a random number for average profit %)
+
+    // 4. Calculate loss from misconducts (hint: use pow() to calculate doubling loss per misconduct)
+
+    // 5. Calculate gross income
+
+    // 6. Calculate taxes owed based on gross income, state, and year
+
+    // 7. Display the gross income, taxes owed, and net income
+
+    return 0;
+}
